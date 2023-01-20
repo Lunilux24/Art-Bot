111bf920dc464445e03a47652a6453482911ce6f
Lunilux24 <beshryb@shaw.ca>
2023-01-19 20:55:59 -0800

    removed .env

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import tweepy
 import keys
 
-def api():
-    auth = tweepy.OAuthHandler(keys.api_key, keys.api_secret)
-    auth.set_access_token(keys.access_token, keys.access_secret)
-    
-    return tweepy.API(auth)
 
+#def api():
+   # auth = tweepy.OAuthHandler(keys.api_key, keys.api_secret)
+   # auth.set_access_token(keys.access_token, keys.access_secret)
+    
+   # return tweepy.API(auth)
+
+auth = tweepy.OAuth1UserHandler(
+   keys.api_key, keys.api_secret, keys.access_token, keys.access_secret
+)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
